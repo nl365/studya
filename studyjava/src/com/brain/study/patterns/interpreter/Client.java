4d12957f3372bec2d335b62acc037a4163806b44
“nl365” <bystudio365@163.com>
2016-12-26 21:20:38 +0800

    interpret

--- studyjava/src/com/brain/study/patterns/interpreter/Client.java
+++ studyjava/src/com/brain/study/patterns/interpreter/Client.java
@@ -3,7 +3,7 @@ package com.brain.study.patterns.interpreter;
 public class Client {
 
 	public static void main(String[] args) {
-		int result = new Minus().interpret((new Context(new Plus().interpret(new Context(9, 2)), 8)));
+		int result = new Minus().interpret(new Context(new Plus().interpret(new Context(9, 2)), 8));
 		System.out.println(result);
 	}
 
